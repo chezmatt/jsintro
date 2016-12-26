06bd31f20923129e85495cec3ccbc369da222986
Chezmatt <wilkinson.matthew@gmail.com>
2016-12-25 18:34:38 -0800

    updates

---
+++ students.js
@@ -0,0 +1,19 @@
+var users = {
+ 'Students': [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+  ],
+ 'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+  ]
+ }
+function printUsers(arr){
+ for (var i=0; i<arr.length;i++){
+
+ 	console.log(arr[i].first_name+' '+this.lastname)
+
+
+}
